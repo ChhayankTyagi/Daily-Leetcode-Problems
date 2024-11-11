1aae3ff6784d69ee355f5cb63a3429c8f683bfe8
ChhayankTyagi <88973228+ChhayankTyagi@users.noreply.github.com>
2024-11-11 11:45:31 +0530

    Time: 3 ms (67.88%) | Memory: 28.4 MB (66.45%) - LeetSync

---
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    bool primeSubOperation(vector<int>& nums) {
+        int largest= *max_element(nums.begin(),nums.end());
+        // find max of array to pre-calculate the prime numbers
+        vector<bool> primeSieve(largest+1,false);
+        primeSieve[0]=primeSieve[1]=true;
+        for(int i=2;i*i<=largest;++i){
+            if(!primeSieve[i]){
+                for(int j=2*i;j<=largest;j+=i){
+                    primeSieve[j]=true;
+                }
+            }
+        }
+        // Calculate Prime Numbers
+        vector<int>prime;
+        for(int i=2;i<primeSieve.size();++i){
+            if(!primeSieve[i]){
+                prime.push_back(i);
+            }
+        }
+
+        int index=findMaxSub(prime,0,nums[0]);
+        if(index!=-1){
+            nums[0]-=prime[index];
+        }
+        for(int i=1;i<nums.size();++i){
+            index=findMaxSub(prime,nums[i-1],nums[i]);
+        if(index == -1 && nums[i]<=nums[i-1]){
+            return false;
+        }
+        else if(index!=-1){
+            nums[i]-=prime[index];
+        }
+        }
+        return true;
+    }
+
+    private:
+    int findMaxSub(const vector<int>&primes,int prev,int cur){
+        if(primes.empty()){
+            return -1;
+        }
+        int left=0;
+        int right=primes.size()-1;
+        while(left <= right){
+            int mid=left+(right-left)/2;
+            if(cur-primes[mid]<=prev){
+                right=mid-1;
+            }
+            else{
+                if(mid==primes.size()-1 || cur-primes[mid+1]<=prev){
+                    return mid;
+                }
+                else{
+                    left =mid+1;
+                }
+            }
+        }
+        int mid=right;
+        return (mid >=0 && cur-primes[mid]<=prev)? -1:mid;
+    }
+};
