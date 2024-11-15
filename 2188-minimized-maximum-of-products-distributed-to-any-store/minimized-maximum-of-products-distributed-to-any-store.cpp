7a38bcbaee17d1380fb832bd575a2d3c177c01a0
ChhayankTyagi <88973228+ChhayankTyagi@users.noreply.github.com>
2024-11-15 12:39:22 +0530

    Time: 32 ms (74.63%) | Memory: 87.2 MB (72.39%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
-    int minimizedMaximum(int n, vector<int>& quantities) {
-        // brute-force approach (linear-traversal)
-        // int maxquant=*max_element(quantities.begin(),quantities.end());
-        // for(int i=1;i<maxquant;i++){
-        //     int x=i;
-        //     int stores=0;
-            
-        // for (int q : quantities) {
-        //     stores += (q + x - 1) / x; // Equivalent to ceil(q / x)
-        // }
+    bool possible(int x,vector<int>& quantities,int shops){
+        for(int &products : quantities){
+            shops -= (products+x-1)/x;
+            // ciel(products/x)
+            if(shops<0)return false;
+            // agar store khatam ho gaye pehle hi
 
-        // if(stores<=n)  
-        // return x;
-        
-        // }
-        // return 1;
-           int left = 1;
-    int right = *max_element(quantities.begin(), quantities.end());
-
-    while (left < right) {
-        int mid = left + (right - left) / 2;
-        int storesNeeded = 0;
-        
-        // Calculate the total number of stores needed for this mid value
-        for (int q : quantities) {
-            storesNeeded += (q + mid - 1) / mid; // Equivalent to ceil(q / mid)
-        }
-        
-        if (storesNeeded <= n) {
-            // If stores needed is within our limit, try a smaller max limit
-            right = mid;
-        } else {
-            // If more stores are needed, increase the max limit
-            left = mid + 1;
-        }
+        }return  true;
     }
-    
-    return left; // `left` is now the minimized maximum x
-
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int left=1;
+        int  right=*max_element(quantities.begin(),quantities.end());
+             int  res=0;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+       
+            if(possible(mid,quantities,n)){
+                res=mid;
+                right=mid-1;
+            }
+            else left=mid+1;
+        }return res;
     }
 };
